eb010c4b2118e239a5573628114aa33ab4b942a1
nguyenthanhtb98 <nguyenthanhtb98@gmail.com>
2019-06-24 10:34:37 +0700

    Add project files.

---
+++ ShopBanChuot/Areas/Admin/Controllers/QLTaiKhoanController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ShopBanChuot.Models;
+namespace ShopBanChuot.Areas.Admin.Controllers
+{
+    public class QLTaiKhoanController : Controller
+    {
+        private ShopBanChuotEntities db = new ShopBanChuotEntities();
+
+        // GET: /Admin/QLTaiKhoan/
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View(db.TAIKHOANs.ToList());
+        }
+
+        // GET: /Admin/QLTaiKhoan/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TAIKHOAN taikhoan = db.TAIKHOANs.Find(id);
+            if (taikhoan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taikhoan);
+        }
+
+        // GET: /Admin/QLTaiKhoan/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Admin/QLTaiKhoan/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="TenTaiKhoan,MatKhau,TenKhachHang,GioiTinh,SDT,Email,DiaChi")] TAIKHOAN taikhoan)
+        {
+            TAIKHOAN TK = db.TAIKHOANs.Find(taikhoan.TenTaiKhoan);
+            if(TK!= null)
+            {
+                ViewBag.ThongBaoTenTK = "Tên tài khoản đã tồn tại";
+                return View();
+            }
+            if (ModelState.IsValid)
+            {
+                db.TAIKHOANs.Add(taikhoan);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(taikhoan);
+        }
+
+        // GET: /Admin/QLTaiKhoan/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TAIKHOAN taikhoan = db.TAIKHOANs.Find(id);
+            if (taikhoan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taikhoan);
+        }
+
+        // POST: /Admin/QLTaiKhoan/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="TenTaiKhoan,MatKhau,TenKhachHang,GioiTinh,SDT,Email,DiaChi")] TAIKHOAN taikhoan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(taikhoan).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(taikhoan);
+        }
+
+        // GET: /Admin/QLTaiKhoan/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TAIKHOAN taikhoan = db.TAIKHOANs.Find(id);
+            if (taikhoan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(taikhoan);
+        }
+
+        // POST: /Admin/QLTaiKhoan/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            TAIKHOAN taikhoan = db.TAIKHOANs.Find(id);
+            db.TAIKHOANs.Remove(taikhoan);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ShopBanChuot/Controllers/NguoiDungController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShopBanChuot.Models;
+
+namespace ShopBanChuot.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        //
+        // GET: /NguoiDung/
+        ShopBanChuotEntities db = new ShopBanChuotEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DangKy(TAIKHOAN TK)
+        {
+            if(ModelState.IsValid)
+            {
+                string strTenTK = TK.TenTaiKhoan.Trim();
+                TAIKHOAN TaiKhoan = db.TAIKHOANs.Find(strTenTK);
+                if(TaiKhoan == null) //nếu tài khoản chưa tồn tại
+                {
+                    db.TAIKHOANs.Add(TK);
+                    //thêm vào cơ sở dữ liệu
+                    db.SaveChanges();
+                    ViewBag.DangKyThanhCong = "Đăng ký thành công";
+                }
+                else
+                {
+                    ViewBag.ThongBaoTrungTK = "Tên tài khoản đã tồn tại";
+
+                }
+                return View();
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection f)
+        {
+            string strTenTaiKhoan = f.Get("txtTenTaiKhoan").ToString().Trim();
+            string strMatKhau = f.Get("txtMatKhau").ToString().Trim();
+            TAIKHOAN TK = db.TAIKHOANs.SingleOrDefault(n => n.TenTaiKhoan == strTenTaiKhoan && n.MatKhau == strMatKhau);
+            if(TK!=null)
+            {
+                Session["TaiKhoan"] = TK;
+                Session["TenTaiKhoan"] = TK.TenTaiKhoan;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ThongBao = "Đăng nhập thất bại";
+                return View();
+            }
+        }
+
+        public ActionResult DangXuat()
+        {
+            Session["TaiKhoan"] = null;
+            Session["TenTaiKhoan"] = null;
+            return RedirectToAction("Index", "Home");
+        }
+	}
+}
